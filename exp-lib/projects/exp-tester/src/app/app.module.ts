01206036152558a3a6900be4d3b66587a1e40590
dj-dhwani <dj.spiritualgirl@gmail.com>
2019-02-01 12:06:57 +0530

    exp-lib works!

--- exp-lib/projects/exp-tester/src/app/app.module.ts
+++ exp-lib/projects/exp-tester/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// -- Library imported
+import { ExpLibModule } from "exp-lib";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ExpLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
